30f825856e62853d4be15305886a099fa8c10d1a
hwk0911@gmail.com <hwk0911@gmail.com>
2020-04-11 18:22:56 +0900

    ADD : Goblin Project
    고블린 프로젝트 추가.

---
+++ Assets/Scripts/MoveScripts.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Rigidbody2D rigid;
+    private float moveSpeed = 10.0f;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector2(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = new Vector2(transform.position.x , transform.position.y + moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - moveSpeed * Time.deltaTime);
+        }
+    }
+}
